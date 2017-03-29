1d4d39e8ddcd61aa00d313c362451769af1e8cad
madratman <ratneshmadaan@gmail.com>
2017-03-29 03:58:20 -0400

    updates tensorboardscripts

--- tensorboard_utils/tensorboard_same_tab.sh
+++ tensorboard_utils/tensorboard_same_tab.sh
@@ -1,2 +1,2 @@
-tensorboard --logdir=vanilla_enduro:runs/vanilla_enduro, double_enduro:runs/double_enduro, vanilla_space:runs/vanilla_space, double_space:runs/double_space& > /dev/null 2>&1
+tensorboard --logdir=vanilla_enduro:runs/vanilla_enduro, double_enduro:runs/double_enduro, vanilla_space:runs/vanilla_space, double_space:runs/double_space,q3:runs/q3,q4:runs/q4,q7:runs/q7,linwithoutstuff:runs/linwithoutstuff & > /dev/null 2>&1
 google-chrome http://localhost:6006&
