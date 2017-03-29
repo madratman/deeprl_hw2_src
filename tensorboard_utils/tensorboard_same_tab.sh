1158d249b265be1ce157b1a78696c4e63da6a0c6
madratman <ratneshmadaan@gmail.com>
2017-03-29 12:38:11 -0400

    better names for tensorboard

--- tensorboard_utils/tensorboard_same_tab.sh
+++ tensorboard_utils/tensorboard_same_tab.sh
@@ -1,2 +1,2 @@
-tensorboard --logdir=vanilla_enduro:runs/vanilla_enduro, double_enduro:runs/double_enduro, vanilla_space:runs/vanilla_space, double_space:runs/double_space,q3:runs/q3,q4:runs/q4,q7:runs/q7,linwithoutstuff:runs/linwithoutstuff & > /dev/null 2>&1
+tensorboard --logdir=vanilla_enduro:runs/vanilla_enduro, double_enduro:runs/double_enduro, vanilla_space:runs/vanilla_space, double_space:runs/double_space, linear_vanilla_with:runs/q3, linear_double_Q:runs/q4, duelling:runs/q7, linear_vanilla_WITHOUT:runs/linwithoutstuff & > /dev/null 2>&1
 google-chrome http://localhost:6006&
