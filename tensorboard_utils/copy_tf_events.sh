45534ecc517a579eeef47d2f56572e8e13ad97ed
madratman <ratneshmadaan@gmail.com>
2017-04-02 02:15:32 -0400

    update tensorboard

--- tensorboard_utils/copy_tf_events.sh
+++ tensorboard_utils/copy_tf_events.sh
@@ -5,7 +5,7 @@ mkdir -p runs/q5
 mkdir -p runs/q6
 mkdir -p runs/q7
 
-scp ratneshm@perceptron.ri.cmu.edu:/data/datasets/ratneshm/deeprl_hw2/q2/SpaceInvaders-v0/vanilla/2017-04-01_21-26-14/events.out* runs/q2
+scp ratneshm@perceptron.ri.cmu.edu:/data/datasets/ratneshm/deeprl_hw2/q2/SpaceInvaders-v0/vanilla/2017-04-02_06-14-38/events.out* runs/q2
 scp ratneshm@perceptron.ri.cmu.edu:/data/datasets/ratneshm/deeprl_hw2/q3/SpaceInvaders-v0/vanilla/2017-04-01_21-26-48/events.out* runs/q3
 scp ratneshm@perceptron.ri.cmu.edu:/data/datasets/ratneshm/deeprl_hw2/q4/SpaceInvaders-v0/double/2017-04-01_21-20-43/events.out* runs/q4
 scp ratneshm@perceptron.ri.cmu.edu:/data/datasets/ratneshm/deeprl_hw2/q7/SpaceInvaders-v0/vanilla/2017-04-01_21-20-43/events.out* runs/q7
